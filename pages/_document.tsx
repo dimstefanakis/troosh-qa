de8ee27450090d3aa0f25e764582ab944e59a130
Dimitris Stefanakis <jimstef@outlook.com>
2022-03-21 03:15:17 +0200

    added twitter tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,9 +37,28 @@ export default class Document extends NextDocument {
             name="twitter:image"
             content="https://questions.troosh.app/logo_new.png"
           />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `!function(e,t,n,s,u,a)
+            {e.twq ||
+              ((s = e.twq =
+                function () {
+                  s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+                }),
+              (s.version = "1.1"),
+              (s.queue = []),
+              (u = t.createElement(n)),
+              (u.async = !0),
+              (u.src = "//static.ads-twitter.com/uwt.js"),
+              (a = t.getElementsByTagName(n)[0]),
+              a.parentNode.insertBefore(u, a))}
+            (window,document,'script'); // Insert Twitter Pixel ID and Standard
+            Event data below twq('init','o85tx'); twq('track','PageView');`,
+            }}
+          />
         </Head>
         <body>
-          {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
